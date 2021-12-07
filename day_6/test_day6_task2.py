86e3d113a8297be709f1397b7ce88daeb0f8fda0
Ole-Kenneth Bratholt <ok@schibsted.com>
2021-12-07 16:53:24 +0100

    Fix Day 6 Task 2 🐡

--- day_6/test_day6_task2.py
+++ day_6/test_day6_task2.py
@@ -5,28 +5,38 @@ from collections import Counter
 class Test_Day6_Task2(unittest.TestCase):
     day = 0
     data = []
-
-    def calc_age(self, age):
-        if age == 0:
-            self.data.append(9)
-            age = 7
-        return age - 1
+    counter = {}
+    prev_round = 0
 
     def day_tick(self):
         self.day += 1
-        subtrack_counter = Counter({ 1:1, 2:1, 3:1, 4:1, 5: 1})
-        self.data.subtract(subtrack_counter)
+        self.prev_round = self.counter['0']
 
+        self.counter = Counter({
+            '0': self.counter['1'],
+            '1': self.counter['2'],
+            '2': self.counter['3'],
+            '3': self.counter['4'],
+            '4': self.counter['5'],
+            '5': self.counter['6'],
+            '6': self.counter['7'] + self.prev_round,
+            '7': self.counter['8'],
+            '8': self.prev_round,
+        })
 
     def test(self):
-        csv = readfile('./day_6/data-small.txt')
-        self.data = Counter(list(map(int, csv[0].split(','))))
-        print(self.data)
-        self.day_tick()
-        print(self.data)
+        csv = readfile('./day_6/data.txt')
+        self.data = list(map(str, csv[0].split(',')))
+        self.counter = Counter(self.data)
 
-        # while self.day < 80:
-        #     #print('After ', self.day, self.data)
-        #     self.day_tick()
-        print('total fish', len(self.data))
-        assert(0 == 0)
+        while self.day < 256:
+            #print('After ', self.day, dict(self.counter))
+            self.day_tick()
+
+        total = 0
+        for d in self.counter:
+            print(d, self.counter[d])
+            total += 1 * self.counter[d]
+
+        print('total', total)
+        assert(total == 1572358335990)
